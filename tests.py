dd4cf8200fef69318bb109af721264200518ed38
janneyt <janneyt@oregonstate.edu>
2020-09-24 16:25:59 -0700

    Added a new function, evaluateAnd, and tests for that function

--- tests.py
+++ tests.py
@@ -4,5 +4,26 @@ from truthTables import *
 
 class testLogic(unittest.TestCase):
 
-    def testAnd(self):
-        self.assertEqual(logicAnd(True, False), ANDTABLE)
+    def testAndTF(self):
+        self.assertEqual(logicAnd(True, False), ':p  :q  :p and q\n:T  :F  :F\n')
+
+    def testAndTT(self):
+        self.assertEqual(logicAnd(True, True), ':p  :q  :p and q\n:T  :T  :T\n')
+
+    def testAndFT(self):
+        self.assertEqual(logicAnd(False, True), ':p  :q  :p and q\n:F  :T  :F\n')
+
+    def testAndFF(self):
+        self.assertEqual(logicAnd(False, False), ':p  :q  :p and q\n:F  :F  :F\n')
+
+    def testEvaluateAndTF(self):
+        self.assertEqual(evaluateAnd(True, False), False)
+
+    def testEvaluateAndTT(self):
+        self.assertEqual(evaluateAnd(True, True), True)
+
+    def testEvaluateAndFF(self):
+        self.assertEqual(evaluateAnd(False, False), False)
+
+    def testEvaluateAndFT(self):
+        self.assertEqual(evaluateAnd(False, True), False)
--- truthTables.py
+++ truthTables.py
@@ -2,5 +2,15 @@ TRUE = 'T'
 FALSE = 'F'
 ANDTABLE = '\n:p  :q  :p and q\n:T  :T  :T\n:T  :F  :F\n:F  :T  :F\n:F  :F  :F\n'
 
-def logicAnd(p, q):
+def evaluateAnd(p, q):
     pass
+
+def logicAnd(p, q):
+    '''
+    Takes a pair of boolean values and returns a string to be printed indicating
+    truth table for that pair of boolean AND expressions.
+
+    Boolean, Boolean -> string
+    '''
+
+    evaluateAnd(p, q)
