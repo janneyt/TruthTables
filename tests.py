3094f6c9e3dfa88cd1f8977b2808432163bdaf49
janneyt <janneyt@oregonstate.edu>
2020-09-28 16:34:00 -0700

    Added tests defining error messages and conditional statements.

--- tests.py
+++ tests.py
@@ -3,6 +3,26 @@ from truthTables import *
 
 
 class testLogic(unittest.TestCase):
+    def testInput(self):
+        self.assertEqual(initInput("a and b"), ANDTABLE)
+
+    def testInputInteger(self):
+        self.assertEqual(initInput(1), getInput(INITERROR))
+
+    def testInputInteger(self):
+        self.assertEqual(initInput("a^b"), getInput(INITERROR))
+
+    def testInputInteger(self):
+        self.assertEqual(initInput("Hello world"), getInput(INITERROR))
+
+    def testInputInteger(self):
+        self.assertEqual(initInput(True), getInput(INITERROR))
+
+    def testInputInteger(self):
+        self.assertEqual(initInput("a adn b"), getInput(INITERROR))
+
+    def testInputInteger(self):
+        self.assertEqual(initInput(INITERROR), getInput(INITERROR))
 
     def testAndTF(self):
         self.assertEqual(logicAnd(True, False), ':p  :q  :p and q\n:T  :F  :F\n')
@@ -17,43 +37,67 @@ class testLogic(unittest.TestCase):
         self.assertEqual(logicAnd(False, False), ':p  :q  :p and q\n:F  :F  :F\n')
 
     def testAndString(self):
-        self.assertEqual(logicAnd("Hi", "World"), ERROR)
+        self.assertEqual(logicAnd("Hi", "World"), getInput(ERROR))
 
     def testAndInteger(self):
-        self.assertEqual(logicAnd(1,2), ERROR)
+        self.assertEqual(logicAnd(1,2), getInput(ERROR))
 
-    def testEvaluateAndTF(self):
-        self.assertEqual(evaluateAnd(True, False), False)
+    def testrepresentAndTF(self):
+        self.assertEqual(representAnd(True, False), False)
 
-    def testEvaluateAndTT(self):
-        self.assertEqual(evaluateAnd(True, True), True)
+    def testrepresentAndTT(self):
+        self.assertEqual(representAnd(True, True), True)
 
-    def testEvaluateAndFF(self):
-        self.assertEqual(evaluateAnd(False, False), False)
+    def testrepresentAndFF(self):
+        self.assertEqual(representAnd(False, False), False)
 
-    def testEvaluateAndFT(self):
-        self.assertEqual(evaluateAnd(False, True), False)
+    def testrepresentAndFT(self):
+        self.assertEqual(representAnd(False, True), False)
 
-    def testEvaluateString(self):
-        self.assertEqual(evaluateAnd("False", "True"), ERROR)
+    def testrepresentString(self):
+        self.assertEqual(representAnd("False", "True"), getInput(ERROR))
 
-    def testEvaluateAndInteger(self):
-        self.assertEqual(evaluateAnd(1,0), ERROR)
+    def testrepresentAndInteger(self):
+        self.assertEqual(representAnd(1,0), getInput(ERROR))
 
-    def testEvaluateOrTT(self):
-        self.assertEqual(evaluateOr(True, True), ':p  :q  :p or q\n:T  :T  :T\n')
+    def testrepresentOrTT(self):
+        self.assertEqual(representOr(True, True), True)
 
-    def testEvaluateOrString(self):
-        self.assertEqual(evaluateOr("True", "False"), ERROR)
+    def testrepresentOrString(self):
+        self.assertEqual(representOr("True", "False"), getInput(ERROR))
 
-    def testEvaluateOrInteger(self):
-        self.assertEqual(evaluateOr(1, 0), ERROR)
+    def testrepresentOrInteger(self):
+        self.assertEqual(representOr(1, 0), getInput(ERROR))
 
-    def testEvaluateFF(self):
-        self.assertEqual(evaluateOr(False, False), False)
+    def testrepresentFF(self):
+        self.assertEqual(representOr(False, False), False)
 
-    def testEvaluateFT(self):
-        self.assertEqual(evaluateOr(False, True), True)
+    def testrepresentFT(self):
+        self.assertEqual(representOr(False, True), True)
 
-    def testEvaluateOrTF(self):
-        self.assertEqual(evaluateOr(True, False), True)
+    def testrepresentOrTF(self):
+        self.assertEqual(representOr(True, False), True)
+
+    def testrepresentConditional(self):
+        self.assertEqual(representConditional("if p then q"), CONDTABLE)
+
+    def testrepresentConditionalMalformed(self):
+        self.assertEqual(representConditional("f p then q"), initInput(CONDERROR))
+
+    def testRepresentConditionalInteger(self):
+        self.assertEqual(representConditional(1), getInput(CONDERROR))
+
+    def testRepresentConditionalBoolean(self):
+        self.assertEqual(representConditional(True), getInput(CONDERROR))
+
+    def testRepresentConditionalContradiction(self):
+        self.assertEqual(representConditional("if p then ~p"), getInput(CONTRACOND))
+
+    def testRepresentConditionalTautology(self):
+        self.assertEqual(representConditional("if p then p"), getInput(CONTRATAUT))
+
+    def testRepresentConditionalMultiple(self):
+        self.assertEqual(representConditional("if p then (if q then r)"), MULTICONTRATABLE)
+
+    def testRepresentConditionalMultiMalformed(self):
+        self.assertEqual(representConditional("if p then if q then r"), getInput(PARAERROR))
--- truthTables.py
+++ truthTables.py
@@ -1,14 +1,49 @@
 TRUE = 'T'
 FALSE = 'F'
 ANDTABLE = '\n:p  :q  :p and q\n:T  :T  :T\n:T  :F  :F\n:F  :T  :F\n:F  :F  :F\n'
-ERROR = "Something went wrong, please enter values again. This program only accepts boolean values in most functions."
+ERROR = ("Something went wrong, please enter values again. This program only"
+        + "accepts boolean values in most functions.")
 
-def evaluateOr(p, q):
+INITERROR = ("Something went wrong with your logical equivalence. Please only"
+        + "enter approved characters. Type -h for usage or to see approved characters.")
+
+CONDTABLE = '\n:p  :q  :p -> q\n:T  :T  :T\n:T  :F  :T\n:F  :F  :T\n:T  :F  :F\n'
+
+CONDERROR = ("Something is wrong with the syntax of your conditional statement."
+        + "Remember that conditionals must be expressed in the form\"if p then q\"."
+        + "Type -h for usage and additional details.")
+
+CONTRACONDTABLE = '\n:p  :q  :p -> q\n:T  :F  :F\n:F  :T  :T\n'
+
+CONTRACOND = ("You have entered a contradiction of the form \"if p then ~p\"."
+        + "Since a conditional is false whenever the hypothesis is true but"
+        + "the conclusion is false, this statement has the following truth table:"
+        + CONTRACONDTABLE
+        + "This leads to a contradiction. Please make sure you entered the"
+        + "correct values. Would you like to reenter your input? Y/n")
+
+CONTRATAUT = ("You have entered a tautology of the form \"if p then p\". This is"
+        + "...rather obvious, don't you think? Did you enter the correct"
+        + "input? Would you like to reenter input? Y/n")
+
+MULTICONTRATABLE = ("\n:p  :q  :r  :p -> (q -> r)\n:T  :T  :T  :T\n:T  :F  :T  "
+        +":T\n:T  :T  :F  :F\n:F  :T  :T  :T\n:F  :F  :T  :T\n:F  :F  :F  :T\n")
+
+PARAERROR = ("You are missing at least one parenthesis. Please adjust your input"
+        + " and make sure you provide parenthesis for all multiple clause expressions.")
+
+def getInput(message):
     pass
 
-def evaluateAnd(p, q):
+def initInput(message):
+    pass
+
+def representOr(p, q):
+    pass
+
+def representAnd(p, q):
     '''
-    Evaluates two booleans in an AND truth table and returns the corresponding
+    represents two booleans in an AND truth table and returns the corresponding
     truth table value.
 
     Boolean, Boolean -> Boolean
@@ -27,7 +62,7 @@ def logicAnd(p, q):
 
     Boolean, Boolean -> string
     '''
-    eval = evaluateAnd(p, q)
+    eval = representAnd(p, q)
     if type(p) != bool or type(q) != bool:
         return ERROR
     return_eval = ':p  :q  :p and q\n:'
@@ -48,3 +83,10 @@ def logicAnd(p, q):
             return ERROR
     else:
         return ERROR
+
+def representConditional(message):
+    '''
+    Takes a conditioal statement and returns a string with the truth table for a conditional.
+
+    String -> String
+    '''
