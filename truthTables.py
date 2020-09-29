943e3ff0bc709684156662255163e87330ed64e5
janneyt <janneyt@oregonstate.edu>
2020-09-28 21:29:14 -0700

    Added docstrings

--- truthTables.py
+++ truthTables.py
@@ -87,9 +87,21 @@ Input Entry and Validation Functions
 ################################################################################
 '''
 def getInput(message):
+    '''
+    Takes a string, delivers it to the user, takes user's input back, and initiates
+    the appropriate action.
+
+    String, Input -> Function call
+    '''
     pass
 
 def initInput(message):
+    '''
+    Takes a string, outputs a welcome screen with the appropriate message, and starts
+    the getInput loop with the user's input.
+
+    String, input -> Function call
+    '''
     pass
 
 '''
@@ -99,11 +111,16 @@ Functions that generate representations of truth tables and logical statements
 '''
 
 def representOr(p, q):
+    '''
+    Takes two booleans and generates the corresponding truth table value.
+
+    Boolean, Boolean -> Boolean
+    '''
     pass
 
 def representAnd(p, q):
     '''
-    represents two booleans in an AND truth table and returns the corresponding
+    Represents two booleans in a AND truth table and returns the corresponding
     truth table value.
 
     Boolean, Boolean -> Boolean
