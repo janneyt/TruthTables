7dc8385d2033d0e111e3c1e0c0088ecd432471cd
Ted Janney <janneyt@oregonstate.edu>
2020-09-29 00:08:45 -0700

    sentry is hidden

--- truthTables.py
+++ truthTables.py
@@ -1,8 +1,10 @@
+'''
 import sentry_sdk
 sentry_sdk.init(
     "https://fa56999182eb46079528c6440bbc6fd4@o454438.ingest.sentry.io/5444367",
     traces_sample_rate=1.0
 )
+'''
 
 TRUE = 'T'
 FALSE = 'F'
